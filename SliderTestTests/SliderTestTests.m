42ee5a579c53d7c01514a2b66f0d9c34036fa044
steve0503 <leehr76@gmail.com>
2014-01-03 15:06:11 +0900

    Initial Commit

---
+++ SliderTestTests/SliderTestTests.m
@@ -0,0 +1,34 @@
+//
+//  SliderTestTests.m
+//  SliderTestTests
+//
+//  Created by SDT-1 on 2014. 1. 3..
+//  Copyright (c) 2014년 steve. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SliderTestTests : XCTestCase
+
+@end
+
+@implementation SliderTestTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
